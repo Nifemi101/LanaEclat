9b147d28b29a3eaa5f9ad43bfdd186c8be156a62
Nifemi <adekogbe08@gmail.com>
2026-04-05 12:19:15 +0100

    changed port

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: true // This allows all hosts
+    allowedHosts: true, // This allows all hosts
+    port: 3000
   }
 })
