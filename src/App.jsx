415954035b173536e8dd496732db1cf821bfe47f
Nifemi <adekogbe08@gmail.com>
2026-04-08 17:36:37 +0100

    Added all route

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ import {
 } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import MainLayout from "./Layout/MainLayout";
+import AbooutPage from "./Pages/AbooutPage";
+import ServicePage from "./Pages/ServicePage";
+import BookingPage from "./Pages/BookingPage";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/about" element={<AbooutPage />} />
+        <Route path="/services" element={<ServicePage />} />
+        <Route path="/booking" element={<BookingPage />} />
       </Route>,
     ),
   );
@@ -19,3 +25,5 @@ const App = () => {
 };
 
 export default App;
+
+ 
