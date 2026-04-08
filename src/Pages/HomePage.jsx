f105f80efd4aecd29f09facf4b83b5bd557792f8
Nifemi <adekogbe08@gmail.com>
2026-04-08 17:38:33 +0100

    added service section component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import MainLayout from "../Layout/MainLayout"
 import Herosection from "../Components/Herosection"
 import AboutSection from "../Components/AboutSection"
+import ServiceSection from "../Components/ServiceSection"
 const HomePage = () => {
   return (
     <>
     <MainLayout />
     <Herosection />
     <AboutSection />
+    <ServiceSection />
     </>
   )
 }
