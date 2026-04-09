c99c9c66a9a660b92c603c1d847c9247b0d04b60
Nifemi <adekogbe08@gmail.com>
2026-04-09 23:46:24 +0100

    fixed logo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link to="/">
               <h2 className="text-[#B04A6B] font-serif italic text-2xl font-bold tracking-wide cursor-pointer">
-                Lana Eclat
+                Lana Éclat
               </h2>
             </Link>
           </div>
