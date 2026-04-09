117d56a135c77c0600fce43f851d6ae662dffda1
Nifemi <adekogbe08@gmail.com>
2026-04-09 22:41:57 +0100

    added new section

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,6 +2,8 @@ import MainLayout from "../Layout/MainLayout"
 import Herosection from "../Components/Herosection"
 import AboutSection from "../Components/AboutSection"
 import ServiceSection from "../Components/ServiceSection"
+import WhySection from "../Components/WhySection"
+import FooterSection from "../Components/FooterSection"
 const HomePage = () => {
   return (
     <>
@@ -9,6 +11,8 @@ const HomePage = () => {
     <Herosection />
     <AboutSection />
     <ServiceSection />
+    <WhySection />
+    <FooterSection />
     </>
   )
 }
