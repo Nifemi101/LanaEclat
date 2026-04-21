067026b2854823b1d1799b343d41714b9274c218
Nifemi <adekogbe08@gmail.com>
2026-04-21 10:52:39 +0100

    fixed

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // Settings for 'npm run dev'
+
   server: {
     port: 3000,
     allowedHosts: true,
   },
-  // Settings for 'npm run preview' (This is what Pxxl uses)
+
   preview: {
     port: 3000,
-    host: true,         // Allows external connections
-    allowedHosts: true, // Prevents the "Blocked host" error
+    host: true,
+    allowedHosts: ["lanaeclat.pxxl.click"],
   },
 });
