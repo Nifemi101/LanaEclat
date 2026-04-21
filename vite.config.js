8547c1d8a7cb17339648c171d5049a6f9ff16de3
Nifemi <adekogbe08@gmail.com>
2026-04-21 10:19:36 +0100

    fixed deployment error

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // Settings for 'npm run dev'
   server: {
     port: 3000,
+    allowedHosts: true,
   },
+  // Settings for 'npm run preview' (This is what Pxxl uses)
   preview: {
     port: 3000,
-    host: true,
-    allowedHosts: true,
+    host: true,         // Allows external connections
+    allowedHosts: true, // Prevents the "Blocked host" error
   },
 });
