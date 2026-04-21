37e4472a899383bd290402fd225c9fe9daeb41e4
Nifemi <adekogbe08@gmail.com>
2026-04-21 20:45:43 +0100

    fixed global port

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000,
+    host: true,
+    port,
   },
   preview: {
-    host: true, 
-    port: 3000,
+    host: true,
+    port,
     allowedHosts: true,
   },
 });
