9216622925b449fa310e0f830c5f290386244fb9
Nifemi <adekogbe08@gmail.com>
2026-04-21 10:07:22 +0100

    fixed port number

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: true, // This allows all hosts
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+    allowedHosts: true,
+  },
+});
